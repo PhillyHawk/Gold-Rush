6924cde77219fe7be70aa5331756c99b54264fef
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-01 22:41:01 -0400

    editing after tutor session

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,57 @@
-
-//establish target number
-
 var targetNumber = Math.floor(Math.random() * (120-19 + 1) + 19);
-$("#number-to-guess").text(targetNumber);
+$("#targetScore").text(targetNumber)
 
-var goldbar = MAth.floor(Math.random() * (12-1 + 1) + 1);
+var goldbar = Math.floor(Math.random() * (12-1 + 1) + 1);
 var counter = 0;
 
 //var numberOptions = [10, 11];
-//var increment = numberOptions[Math.round(Math.random())];
-var winCount = 0;
-var lossCount = 0;
+//var increment = numberOptions[Math.floor(Math.random())];//
+var winCount = 0
+var lossCount = 0
 
 function restartGame(){
   $("targetScore").innerHTML = 0;
   $("currentScore").innerHTML = 0;
 }
 
-//on click event for bars
 
-
-$("#gold").on("click", function() {
-  $("#gold").value = goldbar;
+$(".gold").on("click", function() {
+  $(".gold").value = goldbar;
   counter += goldbar;
   $("#currentScore").text(counter);
- 
-});
+  });
 
 
-$("#silver").on("click", function() {
-  $("#silver").value = goldbar;
-  counter += goldbar;
+$(".silver").on("click", function() {
+  counter += 5;
   $("#currentScore").text(counter);
+  });
 
-});
-
-
-$("#bronze").on("click", function() {
-  $("#silver").value = goldbar;
-  counter += goldbar;
+  
+$(".bronze").on("click", function() {
+  counter += 1;
   $("#currentScore").text(counter);
+  });
 
-
-});
-if (counter === targetnumber) {
+if (counter === targetNumber){
   winCount++;
   $("#winCount").text(winCount);
   alert("Jackpot!");
+  restartGame();
+
 }
-else if (counter >= targetNumber){
+
+else if (counter > targetNumber){
   lossCount++;
   $("#lossCount").text(lossCount);
   alert("Bankrupt!");
+  restartGame();
 }
+
+
+
+
+
+
+
+console.log(counter);
