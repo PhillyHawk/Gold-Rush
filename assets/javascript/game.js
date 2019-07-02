0116e57bbf60ac5f9f6e3288a031b2a958efb70d
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-01 21:38:23 -0400

    adding headers for score board

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,42 @@
 alert("hi");
 //establish target number
+
 var targetNumber = 50;
 $("#number-to-guess").text(targetNumber);
+
 var counter = 0;
+
+var numberOptions = [10, 11];
+var increment = numberOptions[Math.round(Math.random())];
+
 //on click event for bars
+
+
 $("#gold").on("click", function() {
-  counter += 10;
-  alert("you clicked a crystal");
+  counter += incremnts;
+  alert("New Score: " + coounter);
   if (counter === targetnumber) {
     alert("Jackpot!");
   }
+  else if (counter >= targetNumber){
+    alert("you loose");
+}
 });
 
 
-$("#silver").on("click", function() {
-  counter += 5;
-  alert("you clicked a crystal");
-});
+//$("#silver").on("click", function() {
+ // counter += 5;
+  //alert("New Score: " + counter);
+  //if (counter === targetnumber) {
+   // alert("Jackpot!")
+  //}
+//});
 
 
-$("#bronze").on("click", function() {
-  counter += 1;
-  alert("you clicked a crystal");
-});
+//$("#bronze").on("click", function() {
+  //counter += 1;
+  //alert("New Score " + counter);
+  //if (counter === targetnumber) {
+    //alert("Jackpot!")
+  //}
+//});
