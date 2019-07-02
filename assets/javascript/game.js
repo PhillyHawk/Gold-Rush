fca9aa8499749dadf464ce3afd36bb74170b8af2
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-01 21:06:16 -0400

    adding a counter to bars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,18 @@
 alert("hi");
 
+
+
+var counter = 0;
+//on click event for bars
 $("#gold").on("click", function() {
+  counter += 1;
+  alert("you clicked a crystal");
+});
+$("#silver").on("click", function() {
+  counter += 1;
+  alert("you clicked a crystal");
+});
+$("#bronze").on("click", function() {
+  counter += 1;
   alert("you clicked a crystal");
 });
