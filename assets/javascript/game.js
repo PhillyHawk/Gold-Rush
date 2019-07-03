353f83a790d2b8184c75c846cfe3ea5787380cd3
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-03 17:49:25 -0400

    cleaning up files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//globar variables
 var winCount = 0
 var lossCount = 0
 var goldbar = 0
@@ -5,29 +6,31 @@ var silverbar = 0
 var bronzebar = 0
 var counter = 0;
 var targetNumber;
-
+//setting target score to a random value
 function resetBars() {
   targetNumber = Math.floor(Math.random() * (120-19 + 1) + 19);
 $("#targetScore").text(targetNumber)
-
+//setting gold bar values to a random number
  goldbar = Math.floor(Math.random() * (12-1 + 1) + 1);
  silverbar = Math.floor(Math.random() * (12-1 + 1) + 1);
  bronzebar = Math.floor(Math.random() * (12-1 + 1) + 1);
 }
 
 
-
+//resetting target and current score to 0
 function restartGame(){
   $("#targetScore").text("0");
   $("#currentScore").text("0");
   resetBars();
 }
 
-
+//funtion adding clicked score to current score
 $(".gold").on("click", function() {
   $(".gold").value = goldbar;
   counter = counter + goldbar;
   $("#currentScore").text(counter);
+  //checking to see if current score is equal to target score
+  //if it is equal alert player they won and add to win score
   if (counter === targetNumber){
     winCount++;
     $("#winCount").text(winCount);
@@ -35,7 +38,8 @@ $(".gold").on("click", function() {
     restartGame();
   
   }
-  else if (counter >= targetNumber){
+  //if the current score is greater than the target score the alert player they are over and add to loss score
+  else if (counter > targetNumber){
     lossCount++;
     $("#lossCount").text(lossCount);
     alert("Bankrupt!");
@@ -55,7 +59,7 @@ $(".silver").on("click", function() {
   
   }
   
-  else if (counter >= targetNumber){
+  else if (counter > targetNumber){
     lossCount++;
     $("#lossCount").text(lossCount);
     alert("Bankrupt!");
@@ -75,7 +79,7 @@ $(".bronze").on("click", function() {
   
   }
   
-  else if (counter >= targetNumber){
+  else if (counter > targetNumber){
     lossCount++;
     $("#lossCount").text(lossCount);
     alert("Bankrupt!");
@@ -83,7 +87,7 @@ $(".bronze").on("click", function() {
   }
   });
 
-
+//reset game to start mode
 restartGame();
 
 
@@ -92,96 +96,7 @@ restartGame();
 
 
 
-console.log(counter);
 
-
-// var targetNumber =Math.floor(Math.random() * (120 - 19) + 19);
-// $("#targetScore").text(targetNumber)
-
-// // var test2=$("#targetScore").html();
-
-
-
-// var goldbar = Math.floor(Math.random() * (12-1 + 1) + 1);
-// var silverbar = Math.floor(Math.random() * (12-1 + 1) + 1);
-// var bronzebar = Math.floor(Math.random() * (12-1 + 1) + 1);
-// var counter = 0;
-
-
-// var winCount = 0
-// var lossCount = 0
-
-// $(".gold").on("click", function() {
-//   $(".gold").value = goldbar;
-//   counter += goldbar;
-//   $("#currentScore").text(counter);
-//   });
-
-
-// $(".silver").on("click", function() {
-//   counter += silverbar;
-//   $("#currentScore").text(counter);
-//   });
-
-  
-// $(".bronze").on("click", function() {
-//   counter += bronzebar;
-//   $("#currentScore").text(counter);
-//   });
-  
-  
-//   function restartGame(){
-//     $("targetScore").innerHTML = 0;
-//     $("currentScore").innerHTML = 0;
-  
-//   }
-//    function checkGame(){
-//      if (counter ===  Number(test2)){
-//        winCount++;
-//        $("#winCount").text(winCount);
-//        alert("Jackpot!");
-  
-//        restartGame();
-    
-//      }
-//    else {
-//      lossCount++;
-//      $("#lossCount").text(lossCount);
-//      alert("Bankrupt!");
-//      restartGame();
-//    }
-//  }
-
-
-// var test=$("#targetScore").val()
-// let test3 = $("#currentScore").html();
-// console.log("This is test2:" + Number(test2));
-// console.log("This is counter: " + Number(test3));
-
-
-// console.log("This is the type of counter: " + typeof Number(test3));
-// console.log("This is the type of test2: " + typeof Number(test2));
-
-
-// console.log(counter === Number(test2))
-// console.log(counter > Number(test2));
-
-// if (counter ===  Number(test2)){
-//   winCount++;
-//   $("#winCount").text(winCount);
-//   alert("Jackpot!");
-//   $("targetScore").innerHTML = 0;
-//   $("currentScore").innerHTML = 0;
-
-// }
-
-// else {
-//   lossCount++;
-//   $("#lossCount").text(lossCount);
-//   alert("Bankrupt!");
-//   $("targetScore").innerHTML = 0;
-//   $("currentScore").innerHTML = 0;
-// }
 
 
 
