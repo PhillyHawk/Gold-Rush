4c9d8118b51fb372d8d60a44978fef40de88aad9
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-02 20:08:41 -0400

    font shadowing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,45 @@
-var targetNumber = Math.floor(Math.random() * (120-19 + 1) + 19);
+
+
+
+var targetNumber =Math.floor(Math.random() * (120 - 19) + 19);
 $("#targetScore").text(targetNumber)
 
+var test2=$("#targetScore").html();
+
+
+
 var goldbar = Math.floor(Math.random() * (12-1 + 1) + 1);
+var silverbar = Math.floor(Math.random() * (12-1 + 1) + 1);
+var bronzebar = Math.floor(Math.random() * (12-1 + 1) + 1);
 var counter = 0;
 
-//var numberOptions = [10, 11];
-//var increment = numberOptions[Math.floor(Math.random())];//
+
 var winCount = 0
 var lossCount = 0
 
 function restartGame(){
   $("targetScore").innerHTML = 0;
   $("currentScore").innerHTML = 0;
+
 }
+// function checkGame(){
+//   if (counter ===  Number(test2)){
+//     winCount++;
+//     $("#winCount").text(winCount);
+//     alert("Jackpot!");
+
+//     restartGame();
+  
+//   }
+  
+//   else {
+//     lossCount++;
+//     $("#lossCount").text(lossCount);
+//     alert("Bankrupt!");
+//     restartGame();
+//   }
+// }
+
 
 
 $(".gold").on("click", function() {
@@ -23,35 +50,49 @@ $(".gold").on("click", function() {
 
 
 $(".silver").on("click", function() {
-  counter += 5;
+  counter += silverbar;
   $("#currentScore").text(counter);
   });
 
   
 $(".bronze").on("click", function() {
-  counter += 1;
+  counter += bronzebar;
   $("#currentScore").text(counter);
   });
 
-if (counter === targetNumber){
-  winCount++;
-  $("#winCount").text(winCount);
-  alert("Jackpot!");
-  restartGame();
-
-}
-
-else if (counter > targetNumber){
-  lossCount++;
-  $("#lossCount").text(lossCount);
-  alert("Bankrupt!");
-  restartGame();
-}
+var test=$("#targetScore").val()
+let test3 = $("#currentScore").val();
+console.log("This is test2:" + Number(test2));
+console.log("This is counter: " + Number(test3));
 
 
+console.log("This is the type of counter: " + typeof Number(test3));
+console.log("This is the type of test2: " + typeof Number(test2));
+
+
+console.log(counter === Number(test2))
+console.log(counter > Number(test2));
+
+// if (counter ===  Number(test2)){
+//   winCount++;
+//   $("#winCount").text(winCount);
+//   alert("Jackpot!");
+//   $("targetScore").innerHTML = 0;
+//   $("currentScore").innerHTML = 0;
+
+// }
+
+// else {
+//   lossCount++;
+//   $("#lossCount").text(lossCount);
+//   alert("Bankrupt!");
+//   $("targetScore").innerHTML = 0;
+//   $("currentScore").innerHTML = 0;
+// }
 
 
 
 
 
-console.log(counter);
+
+
